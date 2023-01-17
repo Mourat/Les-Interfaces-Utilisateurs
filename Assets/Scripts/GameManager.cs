bd24005af5005f29b8a6716c3b6a5fb6b0085efe
Mourat <mourat@boutry.me>
2023-01-17 12:27:18 +0100

    Create a coroutine to spawn objects

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,7 +1,32 @@
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class GameManager : MonoBehaviour
 {
     [SerializeField] private List<GameObject> targets;
+    private float _spawnRate;
+
+    private void Awake()
+    {
+        _spawnRate = 1f;
+        
+    }
+
+    private void Start()
+    {
+        StartCoroutine(SpawnTarget());
+    }
+
+    IEnumerator SpawnTarget()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(_spawnRate);
+            var index = Random.Range(0, targets.Count);
+            Instantiate(targets[index]);
+        }
+    }
 }
