12669ecba6d8cdbe057e6de26bfc6011dbb8096c
Mourat <mourat@boutry.me>
2023-01-17 11:43:38 +0100

    Toss objects randomly in the air

--- Assets/Scripts/Targets.cs
+++ Assets/Scripts/Targets.cs
@@ -2,5 +2,17 @@ using UnityEngine;
 
 public class Targets : MonoBehaviour
 {
-    
+    private Rigidbody _targetRb;
+
+    private void Awake()
+    {
+        _targetRb = GetComponent<Rigidbody>();
+    }
+
+    private void Start()
+    {
+        _targetRb.AddForce(Vector3.up * Random.Range(12, 16), ForceMode.Impulse);
+        _targetRb.AddTorque(Random.Range(-10, 10), Random.Range(-10, 10), Random.Range(-10, 10), ForceMode.Impulse);
+        transform.position = new Vector3(Random.Range(-4, 4), -6);
+    }
 }
